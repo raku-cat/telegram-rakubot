6115eb4917a22ca75e64bbc05eb84db1856727be
Raku-cat <raku@rakutiki.tv>
2017-04-03 19:42:37 -0500

    Supports quotes now

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ if not os.path.exists(memedir):
 memeindex = memedir + 'memeindex.json'
 if not os.path.exists(memeindex):
     with open(memeindex, 'w') as f:
-        json.dump({'files' : {}, 'quotes' : {} }, f)
+        json.dump({'files' : {}, 'quotes' : {'mtype' : 'quote'} }, f)
 
 
 def handler(msg):
@@ -60,33 +60,61 @@ def handler(msg):
                             except KeyError:
                                 try:
                                     reply['chat']
+                                    authname = '@' + reply['from']['username']
+                                    quotetext = reply['text']
+                                    mtype = 'quote'
+                                    print(authname)
+                                    print(quotetext)
                                 except KeyError:
                                     bot.sendMessage(chat_id, 'Idk what that is, i can\'t grab it')
-                    file_url = 'https://api.telegram.org/file/bot' + key['telegram'] + '/' + bot.getFile(file_id)['file_path']
-                    #print(file_url)
-                    ext = file_url.split('.')[3]
-                    ext = '.' + ext
-                    namevar = datetime.datetime.now().strftime("%Y%m%d%H%M%f") + ext
-                    memedex = { mem : { 'filename' : namevar, 'mtype' : mtype } }
-                    #print(memedex)
-                    with open(memeindex) as f:
-                        memefeed = json.load(f)
-                    try:
-                        memefeed['files'][mem]
-                        bot.sendMessage(chat_id, 'Mem already exist :V', reply_to_message_id=msg_id)
-                        return
-                    except KeyError:
-                        pass
-                    memefeed['files'].update(memedex)
-                    with open(memeindex, 'w') as f:
-                        json.dump(memefeed, f, indent=2)
-                    response = requests.get(file_url, stream=True)
-                    with open(memedir + namevar, 'wb') as out_file:
-                        shutil.copyfileobj(response.raw, out_file)
-                    del response
-                    bot.sendMessage(chat_id, 'Meme stored, send with `/meme ' + mem + '`', parse_mode='Markdown', reply_to_message_id=msg_id)
-            except UnboundLocalError:
-                print('beep')
+                    if mtype in ['video', 'audio', 'photo']:
+                        file_url = 'https://api.telegram.org/file/bot' + key['telegram'] + '/' + bot.getFile(file_id)['file_path']
+                        ext = file_url.split('.')[3]
+                        ext = '.' + ext
+                        namevar = datetime.datetime.now().strftime("%Y%m%d%H%M%f") + ext
+                        memedex = { mem : { 'filename' : namevar, 'mtype' : mtype } }
+                        with open(memeindex) as f:
+                            memefeed = json.load(f)
+                        try:
+                            memefeed['files'][mem]
+                        except KeyError:
+                            try:
+                                memefeed['quotes'][mem]
+                                bot.sendMessage(chat_id, 'Mem already exist :V', reply_to_message_id=msg_id)
+                                return
+                            except KeyError:
+                                pass
+                        memefeed['files'].update(memedex)
+                        with open(memeindex, 'w') as f:
+                            json.dump(memefeed, f, indent=2)
+                        response = requests.get(file_url, stream=True)
+                        with open(memedir + namevar, 'wb') as out_file:
+                            shutil.copyfileobj(response.raw, out_file)
+                        del response
+                        bot.sendMessage(chat_id, 'Meme stored, meme with `/meme ' + mem + '`', parse_mode='Markdown', reply_to_message_id=msg_id)
+                    elif mtype == 'quote':
+                        memedex = { mem : { 'text' : quotetext, 'author' : authname } }
+                        with open(memeindex) as f:
+                            memefeed = json.load(f)
+                        try:
+                            memefeed['quotes'][mem]
+                        except KeyError:
+                            try:
+                                memefeed['files'][mem]
+                                bot.sendMessage(chat_id, 'Mem already exist :V', reply_to_message_id=msg_id)
+                                return
+                            except KeyError:
+                                pass
+                        memefeed['quotes'].update(memedex)
+                        with open(memeindex, 'w') as f:
+                            json.dump(memefeed, f, indent=2)
+                        bot.sendMessage(chat_id, 'Meme stored, meme with `/meme ' + mem + '`', parse_mode='Markdown', reply_to_message_id=msg_id)
+            #        else:
+            #            bot.sendMessage(chat_id, 'Something went wrong :(', reply_to_message_id=msg_id)
+            #            return
+            except KeyError:
+                    bot.sendMessage(chat_id, 'Something went wrong :(', reply_to_message_id=msg_id)
+                    return
         elif command.startswith('/meme'):
             try:
                 mem = command.split(' ', 1)[1]
@@ -97,20 +125,33 @@ def handler(msg):
             try:
                 memekey = memefeed['files'][mem]
             except KeyError:
-                bot.sendMessage(chat_id, 'Meme not found', reply_to_message_id=msg_id)
-                return
+                try:
+                    memekey = memefeed['quotes'][mem]
+                except KeyError:
+                        bot.sendMessage(chat_id, 'Meme not found', reply_to_message_id=msg_id)
+                        return
             bot.sendChatAction(chat_id, 'typing')
-            memtype = memekey['mtype']
-            meme = open(memedir + memekey['filename'], 'rb')
-            if memtype == 'audio':
-                bot.sendAudio(chat_id, meme, reply_to_message_id=reply_id)
-            elif memtype == 'video':
-                bot.sendVideo(chat_id, meme, reply_to_message_id=reply_id)
-            elif memtype == 'image':
-                bot.sendPhoto(chat_id, meme, reply_to_message_id=reply_id)
-            else:
-                bot.sendMessage(chat_id, 'Something went wrong :(', reply_to_message_id=msg_id)
-                return
+            try:
+                memtype = memekey['mtype']
+            except KeyError:
+                pass
+            try:
+                if memtype in  ['video', 'audio', 'photo']:
+                    try:
+                        meme = open(memedir + memekey['filename'], 'rb')
+                    except:
+                        bot.sendMessage(chat_id, 'Something went wrong :(', reply_to_message_id=msg_id)
+                if memtype == 'audio':
+                    bot.sendAudio(chat_id, meme, reply_to_message_id=reply_id)
+                elif memtype == 'video':
+                    bot.sendVideo(chat_id, meme, reply_to_message_id=reply_id)
+                elif memtype == 'photo':
+                    bot.sendPhoto(chat_id, meme, reply_to_message_id=reply_id)
+                else:
+                    bot.sendMessage(chat_id, 'Something went wrong :(', reply_to_message_id=msg_id)
+                    return
+            except UnboundLocalError:
+                bot.sendMessage(chat_id, memekey['text'] + '\n  _-' + memekey['author'] + '_', reply_to_message_id=reply_id, parse_mode='Markdown')
         else:
             return
     else:
