1f82ab2d6e72082e2019970067e922b072f0473c
raku-cat <raku@rakutiki.tv>
2017-05-12 21:01:33 +0000

    cleaned some things up

--- bot.py
+++ bot.py
@@ -54,94 +54,91 @@ async def on_command(msg):
             except IndexError:
                 await bot.sendMessage(chat_id, 'Expected second argument as name `/store <name>`', parse_mode='Markdown')
                 return
-            try:
-                if reply:
+            if reply:
+                typeaud = reply.get('audio', {}).get('file_id')
+                typepic = reply.get('photo', {})
+                typedoc = reply.get('document', {}).get('file_id')
+                typechat = reply.get('chat')
+                typevoice = reply.get('voice', {}).get('file_id')
+                if typeaud is not None:
+                    mtype = 'audio'
+                    file_id = typeaud
+                elif typepic is not None:
+                    mtype = 'photo'
+                    file_id = typepic[-1]['file_id']
                     try:
-                        file_id = reply['audio']['file_id']
+                        caption = reply['caption']
+                    except KeyError:
+                        caption = ''
+                elif typedoc is not None:
+                    file_id = typedoc
+                    dmtye = reply['document']['mime_type'].split('/')[0]
+                    if dmtye == 'audio':
                         mtype = 'audio'
-                    except KeyError:
+                    elif dmtye == 'video':
+                        mtype = 'video'
                         try:
-                            file_id = reply['photo'][-1]['file_id']
-                            mtype = 'photo'
-                            try:
-                                caption = reply['caption']
-                            except KeyError:
-                                caption = ''
+                            caption = reply['caption']
                         except KeyError:
-                            try:
-                                file_id = reply['document']['file_id']
-                                dmtye = reply['document']['mime_type'].split('/')[0]
-                                if dmtye == 'audio':
-                                    mtype = 'audio'
-                                elif dmtye == 'video':
-                                    mtype = 'video'
-                                    try:
-                                        caption = reply['caption']
-                                    except KeyError:
-                                        caption = ''
-                            except KeyError:
-                                try:
-                                    reply['chat']
-                                    authname = '@' + reply['from']['username']
-                                    quotetext = reply['text']
-                                    mtype = 'quote'
-                                except KeyError:
-                                    try:
-                                        file_id = reply['voice']['file_id']
-                                        mtype = 'audio'
-                                    except KeyError:
-                                        await bot.sendMessage(chat_id, 'Idk what that is, i can\'t grab it')
-                                        return
-                    async with aiofiles.open(memeindex) as f:
-                        memefeed = json.loads(await f.read())
-                    try:
-                        memefeed['files'][mem]
-                        await bot.sendMessage(chat_id, 'Mem already exist :V', reply_to_message_id=msg_id)
-                        return
-                    except KeyError:
-                        try:
-                            memefeed['quotes'][mem]
-                            await bot.sendMessage(chat_id, 'Mem already exist :V', reply_to_message_id=msg_id)
-                            return
-                        except KeyError:
-                            pass
-                    if mtype in ['video', 'audio', 'photo']:
-                        file_path = await bot.getFile(file_id)
-                        file_url = 'https://api.telegram.org/file/bot' + key['telegram'] + '/' + file_path['file_path']
-                        try:
-                            ext = '.' + file_url.split('.')[3]
-                        except IndexError:
-                            ext = ''
-                        if mtype == 'audio':
-                            ext = '.ogg'
-                        namevar = datetime.datetime.now().strftime("%Y%m%d%H%M%f") + ext
-                        memedex = { mem : { 'filename' : namevar, 'mtype' : mtype, 'cap' : caption } }
-                        memefeed['files'].update(memedex)
-                        async with aiohttp.ClientSession() as session:
-                            async with session.get(file_url) as r:
-                                if r.status == 200:
-                                    async with aiofiles.open(memedir + namevar, 'wb') as f:
-                                        while True:
-                                            chunk = await r.content.read(128)
-                                            if not chunk:
-                                                break
-                                            await f.write(chunk)
-                                else:
-                                    await bot.sendMessage(chat_id, 'Telegram is messing up, I can\'t do anything about it sorry', reply_to_message_id=msg_id)
-                                    return
-                    elif mtype == 'quote':
-                        memedex = { mem : { 'text' : quotetext, 'author' : authname } }
-                        memefeed['quotes'].update(memedex)
-                    with open(memeindex, 'w') as f:
-                        json.dump(memefeed, f, indent=2)
-                    await bot.sendMessage(chat_id, 'Meme stored, meme with `/meme ' + mem + '`', parse_mode='Markdown', reply_to_message_id=msg_id)
-                    if mtype == 'video':
-                        filefixer.mp4thumb()
-                    elif mtype == 'audio':
-                        filefixer.oggconv()
+                            caption = ''
+                elif typechat is not None:
+                    authname = '@' + reply['from']['username']
+                    quotetext = reply['text']
+                    mtype = 'quote'
+                elif typevoice is not None:
+                    mtype = 'audio'
+                    file_id = typevoice
+                else:
+                    await bot.sendMessage(chat_id, 'Idk what that is, i can\'t grab it')
                     return
-            except UnboundLocalError:
+            async with aiofiles.open(memeindex) as f:
+                memefeed = json.loads(await f.read())
+            try:
+                memefeed['files'][mem]
+                await bot.sendMessage(chat_id, 'Mem already exist :V', reply_to_message_id=msg_id)
                 return
+            except KeyError:
+                try:
+                    memefeed['quotes'][mem]
+                    await bot.sendMessage(chat_id, 'Mem already exist :V', reply_to_message_id=msg_id)
+                    return
+                except KeyError:
+                    pass
+            if mtype in ['video', 'audio', 'photo']:
+                file_path = await bot.getFile(file_id)
+                file_url = 'https://api.telegram.org/file/bot' + key['telegram'] + '/' + file_path['file_path']
+                try:
+                    ext = '.' + file_url.split('.')[3]
+                except IndexError:
+                    ext = ''
+                if mtype == 'audio':
+                    ext = '.ogg'
+                namevar = datetime.datetime.now().strftime("%Y%m%d%H%M%f") + ext
+                memedex = { mem : { 'filename' : namevar, 'mtype' : mtype, 'cap' : caption } }
+                memefeed['files'].update(memedex)
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(file_url) as r:
+                        if r.status == 200:
+                            async with aiofiles.open(memedir + namevar, 'wb') as f:
+                                while True:
+                                    chunk = await r.content.read(128)
+                                    if not chunk:
+                                        break
+                                    await f.write(chunk)
+                        else:
+                            await bot.sendMessage(chat_id, 'Telegram is messing up, I can\'t do anything about it sorry', reply_to_message_id=msg_id)
+                            return
+            elif mtype == 'quote':
+                memedex = { mem : { 'text' : quotetext, 'author' : authname } }
+                memefeed['quotes'].update(memedex)
+            with open(memeindex, 'w') as f:
+                json.dump(memefeed, f, indent=2)
+                await bot.sendMessage(chat_id, 'Meme stored, meme with `/meme ' + mem + '`', parse_mode='Markdown', reply_to_message_id=msg_id)
+                if mtype == 'video':
+                    filefixer.mp4thumb()
+                elif mtype == 'audio':
+                    filefixer.oggconv()
+                    return
         elif command.startswith('/meme'):
             try:
                 mem = command.split(' ', 1)[1]
